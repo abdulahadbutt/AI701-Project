42aca60528a46bd1be7cbfeea03efd50bdca0022
Abdul Ahad <abhadbutt.pk@gmail.com>
2024-11-21 13:18:34 +0000

    finished llava run on updated custom qs

--- run_llava.sh
+++ run_llava.sh
@@ -1,3 +1,7 @@
 conda activate llava
 python src/llava_exp.py --question-file=custom --answers-file="llava-1.5-13b-hf-custom.jsonl"
 python src/llava_exp.py --question-file=original --answers-file="llava-1.5-13b-hf-mmvp.jsonl"
+python src/llava_exp.py --model-path='llava-hf/llava-1.5-7b-hf' --answers-file='llava-1.5-7b-hf-custom.jsonl' --question-file=custom 
+python src/llava_exp.py --model-path='llava-hf/llava-1.5-7b-hf' --answers-file='llava-1.5-7b-hf-mmvp.jsonl' --question-file=original 
+# python src/llava_exp.py --model-path='llava-hf/llava-v1.6-vicuna-7b-hf' --answers-file='llava-v1.6-vicuna-7b-hf-mmvp.jsonl' --question-file=original 
+# python src/llava_exp.py --model-path='llava-hf/llava-v1.6-vicuna-7b-hf' --answers-file='llava-v1.6-vicuna-7b-hf-custom.jsonl' --question-file=custom 
